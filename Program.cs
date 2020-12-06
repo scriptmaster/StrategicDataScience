9c0b6b246f5750733ef58dca79304036775b43d9
scriptmaster <msheriffusa@gmail.com>
2020-12-06 13:24:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+Console.WriteLine("Hello");
+
+//CreateHostBuilder(args).Build().Run();
+
+//static IHostBuilder CreateHostBuilder(string[] args) =>
+//        Host.CreateDefaultBuilder(args)
+//            .ConfigureWebHostDefaults(webBuilder =>
+//            {
+//                webBuilder.UseStartup<Startup>();
+//            });
+
+var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+var configuration = builder.Build();
+
+Console.WriteLine($"configuration: {configuration.GetConnectionString("sqlserver")}");
